7d4b65831cbc04d7244a5ab50e0a98e0f381d57e
lawrend <douglas_lawrence@mac.com>
2017-09-29 17:53:44 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+ // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    let dist = street - 42
+    if (dist < 0) {
+        dist = dist * -1
+        return dist
+    } else {
+        return dist
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264
+
+}
+
+function distanceTravelledInFeet(pickUp, dropOff) {
+    let blocks = pickUp - dropOff
+    if (blocks < 0) {
+        blocks = blocks * -1
+        return blocks * 264
+    } else {
+        return blocks * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let dist = distanceTravelledInFeet(start, destination)
+    if (dist <= 400) {
+        return 0
+    } else if (dist > 400 && dist <= 2000) {
+        return dist * .02
+    } else if (dist > 2000 && dist <= 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+}
+
+
+
