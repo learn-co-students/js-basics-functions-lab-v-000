d66c8546540237d80deb4a7c64b529dca9fb551a
Elias Ventura <eliasventura23@gmail.com>
2017-10-03 16:01:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+  if(block > 42){
+    return block - 42
+  } else {
+    return 42 - block
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(starting_block, ending_block) {
+  if(starting_block < ending_block) {
+    return (ending_block - starting_block) * 264;
+  } else {
+    return (starting_block - ending_block) *264;
+  }
+}
+
+function calculatesFarePrice(starting_block, ending_block) {
+  if(distanceTravelledInFeet(starting_block, ending_block) <= 400) {
+    return 0;
+  }
+  else if (distanceTravelledInFeet(starting_block, ending_block) > 400 && distanceTravelledInFeet(starting_block, ending_block) <= 2000) {
+    return (distanceTravelledInFeet(starting_block, ending_block)) * .02;
+  }
+
+  else if (distanceTravelledInFeet(starting_block, ending_block) > 2000 && distanceTravelledInFeet(starting_block, ending_block) < 2500) {
+    return 25;
+
+  }
+  else {
+    return `cannot travel that far`
+  }
+}
