7bda1dcd69c8c17b0cb5bb34b2dfb9f671853024
Cody Kasselman <ckasselman@fordham.edu>
2017-10-05 20:02:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+  const distance = distanceTravelledInFeet(start, end);
+  if (distance >= 2500) {
+    return 'cannot travel that far';
+  } else if (distance >= 2000) {
+    return 25;
+  } else if (distance >= 400) {
+    return distance * .02;
+  } else {
+    return 0;
+  }
+}
