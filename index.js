f09591fb7098e453375bcfd2bfa5520f7c15813e
James Dolan <jwdolan820@gmail.com>
2017-10-17 03:16:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+ if (block >= 42) {
+  return block - 42
+}
+ else {
+  return 42 - block
+ }
+}
+
+
+function distanceFromHqInFeet(block) {
+ return distanceFromHqInBlocks(block)*264;
+}
+
+
+function distanceTravelledInFeet(start, end) {
+ if (end >= start) {
+  return (end - start)*264
+ }
+ else {
+  return (start - end)*264
+ }
+}
+
+
+function calculatesFarePrice(start, end) {
+ const distance = distanceTravelledInFeet(start, end);
+ if (distance <= 400) {
+   return 0;
+ } else if (distance > 400 && distance <= 2000) {
+   return distance*0.02;
+ } else if (distance > 2000 && distance < 2500) {
+   return 25
+ } else {
+   return 'cannot travel that far';
+ }
+}
