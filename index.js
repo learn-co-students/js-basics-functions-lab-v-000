18c766b142e2f2a8456352e24c96297cc51a3686
Jschles1 <jschles1@ramapo.edu>
2017-10-19 18:38:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  if (location < 42) {
+    return 42 - location;
+  } else {
+    return location - 42;
+  }
+}
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs((start - end) * 264);
+}
+
+function calculatesFarePrice(start, end) {
+  let distance = distanceTravelledInFeet(start, end);
+  if (distance > 2500) {
+    return "cannot travel that far"
+  } else if (distance <= 2500 && distance > 2000) {
+    return 25;
+  } else if (distance <= 2000 && distance > 400) {
+    return (distance) * 0.02;
+  } else {
+    return 0;
+  }
+}
