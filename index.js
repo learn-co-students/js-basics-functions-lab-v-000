07789eb757f8c3bc4d14cc01d9883cc6fdc41c49
LS-C <suzy.cho111@gmail.com>
2017-11-11 13:40:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    const hq = 42
+    return (hq > blocks ?  hq - blocks : blocks - hq)
+}
+
+
+function distanceFromHqInFeet(blocks) {
+        const verticalBlocks = distanceFromHqInBlocks(blocks)
+        return verticalBlocks * 264
+}
+
+function distanceTravelledInFeet(startingPoint, endingPoint) {
+    const travelledBlocks = startingPoint - endingPoint
+    return (travelledBlocks < 0 ? (Math.abs(travelledBlocks)) * 264 : travelledBlocks * 264)
+}
+
+function calculatesFarePrice(startingPoint, endingPoint) {
+    const distanceTravelled = distanceTravelledInFeet(startingPoint, endingPoint)
+    switch (true) {
+        case (distanceTravelled < 400) :
+            return 0
+            break;
+        case (distanceTravelled > 400 && distanceTravelled <= 2000) :
+            return distanceTravelled * 0.02
+            break;
+        case (distanceTravelled > 2000 && distanceTravelled < 2500) :
+            return 25
+            break;
+        case (distanceTravelled > 2500) :
+            return 'cannot travel that far'
+            break;
+    }
+}
