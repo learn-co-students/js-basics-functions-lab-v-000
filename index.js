62024ce0c2e3c1543d81b72557766f5d56c50be8
Alexandra Wright <superbiscuit@gmail.com>
2017-11-21 11:02:57 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function calculateVertical(begin, end){
+    return (begin - end) * 264
+}
+
+function distanceFromHqInBlocks(block) {
+   return Math.abs(42 - block)
+}
+
+function distanceFromHqInFeet(block) {
+    let distance = distanceFromHqInBlocks(block)
+    return distance * 264
+    // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function
+}
+
+function distanceTravelledInFeet(begin, end){
+    let distance = calculateVertical(begin, end)
+    return Math.abs(distance)
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400) {
+        return 0
+    }
+    else if (distance > 400 && distance < 2000) {
+        return distance * .02
+    }
+    else if (distance > 2000 && distance < 2500) {
+        return 25
+    }
+    else if ( distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
