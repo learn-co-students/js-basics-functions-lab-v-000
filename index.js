68a0e1e7781fb2292f17d3b85d759faf1dc7afd2
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-27 14:35:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function calculateVertical (start, destination) {
+	return Math.abs(start - destination) * 264;
+}
+function distanceFromHqInBlocks (distance) {
+	return Math.abs(distance - 42);
+}
+
+function distanceFromHqInFeet (distance) {
+	const blocks = distanceFromHqInBlocks(distance);
+	return blocks * 264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+	return calculateVertical(start, destination);
+}
+
+function calculatesFarePrice (start, destination) {
+	const distance = distanceTravelledInFeet(start, destination)
+	if (distance < 400) {		
+		return 0;
+	} else if (distance >= 400 && distance <= 2000) {
+		return distance * 0.02;
+	} else if (distance > 2000 && distance <= 2500) {
+		return 25;
+	} else {
+		return 'cannot travel that far';
+	}
+}
