6ad4a96bf989b30a535b7fa771dfbafa752f296e
Donna <donnamcla@gmail.com>
2017-12-03 21:29:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blockNumber) {
+    if (blockNumber > 42) {
+        return blockNumber - 42;
+    } else {
+        return 42 - blockNumber;
+    }
+}
+
+function distanceFromHqInFeet (blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet (startingBlock, endingBlock) {
+    return Math.abs((endingBlock - startingBlock)) * 264;
+}
+
+function calculatesFarePrice (startingBlock, endingBlock) {
+    let dist = distanceTravelledInFeet(startingBlock, endingBlock);
+    if (dist <= 400) {
+        return 0;
+    } else if ((dist >= 400) && (dist <= 2000)) {
+        return dist * 0.02;
+    } else if ((dist > 2000) && (dist <= 2500)) {
+        return 25;
+    } else if (dist > 2500) {
+        return "cannot travel that far";
+    }
+}
