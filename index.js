cc48a101c4010ba65ac2463aaa57ca5e3e1084fc
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-12-05 12:39:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+  if (street >= 42) {
+    return street - 42
+  } else {
+    return 42 - street
+  }
+}
+
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  return calculateVertical(startBlock, endBlock)
+}
+
+
+function calculatesFarePrice(startBlock, endBlock) {
+  let distance = distanceTravelledInFeet(startBlock, endBlock)
+  if (distance > 2500 ) {
+    return 'cannot travel that far'
+  } else if (distance < 400) {
+    return 0
+  } else if (distance > 400 && distance < 2000) {
+    return distance * .02
+  } else if (distance > 2000) {
+    return 25
+  }
+}
+
+
+function calculateVertical(startBlock, endBlock) {
+  if (startBlock > endBlock) {
+    return (startBlock - endBlock) * 264
+  } else {
+    return (endBlock - startBlock) * 264
+  }
+
+}
