222fa1d354c13426b82227fa21920959ed60111b
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-06 12:49:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  if (block > 42) {
+    return block - 42
+  } else {
+    return 42 - block
+  }
+}
+
+function distanceFromHqInFeet(block) {
+   return distanceFromHqInBlocks(block) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start > end) {
+    return (start - end) * 264
+  } else {
+    return (end - start) * 264
+  }
+}
+
+function calculatesFarePrice(start, end) {
+  let distance = distanceTravelledInFeet(start, end)
+
+  if (distance < 400) {
+      return 0
+  } else if (distance < 2000){
+      return distance * 2 / 100
+  } else if (distance < 2500) {
+      return 25
+  } else {
+      return 'cannot travel that far'
+  }
+}
