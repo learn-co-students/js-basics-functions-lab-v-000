c60a597d3efc507023f41466730abd6b6a747c64
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-06 17:20:25 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+  return Math.abs(blockNumber - 42);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(block1, block2) {
+  return Math.abs((block1 - block2) * 264);
+}
+
+function calculatesFarePrice(block1, block2) {
+  const distance = distanceTravelledInFeet(block1, block2);
+
+  if (distance > 2500) {
+    return 'cannot travel that far';
+  } else if (distance > 2000) {
+    return 25;
+  } else if (distance > 400) {
+    return distance * 0.02;
+  } else {
+    return 0;
+  }
+}
