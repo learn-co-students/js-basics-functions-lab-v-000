32de7f25a64da36f107b2b5699ad0c0090edd2aa
kyleblee <hello.kylelee@gmail.com>
2017-12-27 15:25:06 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location) {
+  return Math.abs(location - 42);
+}
+
+function distanceFromHqInFeet (location) {
+  return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(starting, ending) {
+  return Math.abs(starting - ending) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return distance * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
