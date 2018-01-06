a8ca74f77761a325d371e5460c8c5330e6eae46d
github username <juliannne@gmail.com>
2018-01-06 11:59:37 -0500

    Done.

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function distanceTravelledInFeet(startBlock, endBlock) {
 }
 
 function calculatesFarePrice(start, destination) {
-  let distance = distanceTravelledInFeet(start, destination);
-  if (distance < 400) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
     return 0;
-  } else if (distance > 400 && distance < 2000) {
+  } else if (distance > 400 && distance <= 2000) {
     return distance * 0.02;
   } else if (distance > 2000 && distance < 2500) {
     return 25;
