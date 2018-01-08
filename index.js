afb83cbe724f1ba6008769f1bd2e4c4748b0fd1a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2018-01-07 22:27:59 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  const hq = 42
+  const distance = block - hq
+  if (block < hq) {
+    return distance * -1
+  } else {
+    return distance
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  const distanceInBlocks = distanceFromHqInBlocks(block)
+  return distanceInBlocks * 264
+}
+
+function distanceTravelledInFeet(blockOne, blockTwo) {
+  if (blockOne < blockTwo) {
+    return (blockTwo - blockOne) * 264
+  } else {
+    return (blockOne - blockTwo) * 264
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceTravelled = distanceTravelledInFeet(start, destination)
+  if (distanceTravelled === 264) {
+    return 0
+  } else if ((distanceTravelled >= 400) && (distanceTravelled <= 2000)) {
+    return 0.02 * distanceTravelledInFeet(start, destination)
+  } else if ((distanceTravelled > 2000) && (distanceTravelled < 2500)) {
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
