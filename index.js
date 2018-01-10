8dae519f9961c20a740739a02b3859ccf0eb68b4
dbfarms <matt.hunger@gmail.com>
2018-01-10 19:48:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+
+const hq = 42
+
+function distanceFromHqInBlocks(destination) {
+    if (destination > 42){
+        return destination - hq;
+    } else { 
+        return hq - destination
+    }
+    end 
+}
+
+function distanceFromHqInFeet(destination) {
+    const blocks = distanceFromHqInBlocks(destination)
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(destination, endPoint) {
+     if (endPoint > destination) {
+        const trip = endPoint - destination 
+        return trip * 264;
+     } else {
+        const trip = destination - endPoint
+        return trip * 264;
+     }
+     end 
+    
+}
+
+function calculatesFarePrice(start, destination) {
+    const fare_distance = distanceTravelledInFeet(start, destination) 
+    
+  if (fare_distance <= 400) {
+    return 0;
+  } else if (fare_distance > 400 && fare_distance <= 2000) {
+    return .02 * fare_distance;
+  } else if (fare_distance > 2000 && fare_distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
