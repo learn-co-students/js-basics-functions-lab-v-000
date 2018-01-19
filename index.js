3306f8b9cbf9a8ed0698c7349001aaf2fd8b4d5d
Nour Kodeih <nour.kodeih@gmail.com>
2018-01-19 00:13:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (street) {
+ if (street > 42) {
+   return street - 42
+ }
+
+ else {
+   return 42- street
+ }
+}
+
+function distanceFromHqInFeet (street) {
+  return distanceFromHqInBlocks(street)*264
+}
+
+function distanceTravelledInFeet (a,b) {
+  if (a > b) {
+    return (a-b)*264
+  }
+  else {
+    return (b-a)*264
+  }
+}
+
+function calculatesFarePrice (a,b) {
+  const distance = distanceTravelledInFeet (a,b)
+  if (distance <= 400) {
+    return 0
+  }
+  else if (distance > 400 & distance < 2000) {
+    return distance*.02
+  }
+
+  else if (distance > 2000 && distance < 2500) {
+    return 25
+  }
+
+  else {
+    return "cannot travel that far"
+  }
+
+}
