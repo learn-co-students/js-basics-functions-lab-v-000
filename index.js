bf2b220fd394fca66ccc0ee1ffd1c26b89a54553
bkhartmire <bkhartmire@gmail.com>
2018-01-24 14:30:03 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function calculateVertical(beginning, destination) {
+  return Math.abs((destination - beginning) * 264)
+}
+
+function distanceFromHqInBlocks(location) {
+  return Math.abs(42 - location)
+}
+
+function distanceFromHqInFeet(location) {
+  return (distanceFromHqInBlocks(location) * 264)
+}
+
+function distanceTravelledInFeet(beginning, destination) {
+  return calculateVertical(beginning, destination)
+}
+
+function calculatesFarePrice() {
+
+}
