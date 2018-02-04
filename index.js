0e6a5a7640f331a87429ce5395ba538de543a740
Terri Riggle <codeofarmour@outlook.com>
2018-02-04 19:01:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  if (blocks > 42) {
+    return blocks - 42;
+  } else {
+    return 42 - blocks;
+  }
+}
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+function distanceTravelledInFeet(beginning, destination) {
+  if (beginning > destination) {
+    return (beginning - destination) * 264;
+  } else {
+    return (destination - beginning) * 264;
+  }
+}
+function calculatesFarePrice(beginning, destination) {
+  const distance = distanceTravelledInFeet(beginning, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return distance * .02;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
