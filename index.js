91ce6526c75d9f85c6f1d76d23edca721751aa49
CodeDad <github email address>
2018-02-13 21:13:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-function distanceFromHqInBlocks (block) {
-	if (block > 42) {
-	return block - 42;}
-	else if (block < 42){
-		return 42-block } 
-	}
+// function distanceFromHqInBlocks(blocks) {
+// 	if (blocks > 42) {
+// 		return blocks-42
+// 	} else {
+// 		return 42-blocks
+// 	};
+// };
 
-function distanceFromHqInFeet (block) {
-  return distanceFromHqInBlocks(block) * 264;
-  
-}
+function distanceFromHqInBlocks(blocks){
+	return (blocks > 42) ? blocks - 42 : 42 - blocks
+};
 
-function distanceTravelledInFeet(blockA, blockB) {
-	if (blockA > blockB){
-		const feet =  blockA - blockB ;
-		return feet * 264
-	} else if (blockA < blockB) {
-		const feet =  blockB - blockA ;
-		return feet * 264
-	}
-}
+function distanceFromHqInFeet(distance) {
+	return distanceFromHqInBlocks(distance)*264
+};
 
+function distanceTravelledInFeet(starting, ending){
+	if (starting > ending){
+		return (starting - ending) * 264 
+	} else {
+		return (ending - starting) * 264
+	};
+};
 
-function calculatesFarePrice (start, destination){
-	const distance = distanceTravelledInFeet(start, destination);
+function calculatesFarePrice(start, destination){
+	const distance = distanceTravelledInFeet(start, destination)
+	if (distance < 400){
+			return 0;
+	} else if (distance >= 400 && distance <= 2000) {
+			return distance * .02
+	} else if (distance > 2000 && distance < 2500) {
+			return 25
+	} else {
+			return "cannot travel that far"
+	};  
+};
 
-	if (distance <= 400) {
-		return 0;
-	}
-	else if (distance > 400 && distance <= 2000) {
-		return distance * .02;
-	}
-
-	else if (distance > 2000 && distance <= 2500) {
-		return 25;
-	}	
-	else {
-		return 'cannot travel that far';
-	}
-}
 
