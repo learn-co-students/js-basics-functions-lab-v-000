c1adb6507b150c3e546d9308b7b3e3773fd1232e
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-03-27 00:19:30 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -50,7 +50,8 @@ describe('index.js', function() {
 
     it('charges 2 cents per foot when total feet travelled is between 400 and 2000 in example below is 528', function() {
       // total feet travelled is 528 -> 10.56
-      expect(calculatesFarePrice(34, 32)).to.equal(10.56);
+      // minus the 400 ft -> 2.56 (if first 400ft are supposed to be free)
+      expect(calculatesFarePrice(34, 32)).to.equal(2.56);
     });
 
     it('charges 25 dollars for a distance over 2000 feet', function() {
