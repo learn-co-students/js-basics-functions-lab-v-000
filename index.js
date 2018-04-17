c4b4a5fbb725b63efbb3eb467dbebef964310b0a
woollymammoth1 A <isaacatwood@outlook.com>
2018-04-17 07:14:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  return (blocks > 42) ? (blocks - 42) : (42 - blocks);
+}
+
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(start, end){
+  return ((start < end) ? ((end - start)*264) : ((start - end)*264) );
+}
+
+function calculatesFarePrice(start, destination){
+  if (distanceTravelledInFeet (start, destination) < 400) {
+    return 0;
+  } else if (distanceTravelledInFeet(start, destination) < 2000){
+    return (((distanceTravelledInFeet (start, destination)) - 400) * .02);
+  } else if (distanceTravelledInFeet (start, destination) < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
