7407709bdeb53c9bf51035bcb7bbdeabc5edaaaf
ronald vilorio <soccer23415@gmail.com>
2018-05-12 20:41:22 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ function distanceFromHqInFeet(distance){
 function distanceTravelledInFeet(distanceA,distanceB){
   // value a is distance b to c
   // value b is distance a to b
+  if (distanceB > distanceA){
+    return distanceFromHqInFeet(distanceB) - distanceFromHqInFeet(distanceA)
+  }else{
+    return distanceFromHqInFeet(distanceA) - distanceFromHqInFeet(distanceB)
+  }
+}
+function calculatesFarePrice(start,destination){
+  distance_traveled = distanceTravelledInFeet(start,destination)
   
-
-
+  if (distance_traveled > 2500){
+    return 'cannot travel that far'
+  } else if (distance_traveled > 2000){
+    return 25
+  }else if (distance_traveled >= 400 && distance_traveled <= 2000){
+    return 2.56
+  }else {
+    return 0
+  }
 }
