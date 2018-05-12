134587ca1bffef14ccc37c71a00792630df575fa
ronald vilorio <soccer23415@gmail.com>
2018-05-12 18:59:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+  //  distance - 42
+  return distance >= 42 ? distance - 42 : distance - 26
+
+}
+function distanceFromHqInFeet(distance){
+  return distanceFromHqInBlocks(distance) * 264
+}
+function distanceTravelledInFeet(distanceA,distanceB){
+  // value a is distance b to c
+  // value b is distance a to b
+  
+
+
+}
