a8176147a0d493481c83f6aca4c66334beeaca6b
github username apalski <github email address annette.drapalski@gmail.com>
2018-05-19 10:46:54 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+	const hq = 42;
+	if (hq > destination) {
+		return hq - destination;
+	} else {
+		return destination - hq;	
+	}	
+}
+
+function distanceFromHqInFeet(destination) {
+	blocks = distanceFromHqInBlocks(destination);
+	return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+	if (end > start) {
+		blocks = end - start;
+	} else {
+		blocks = start - end;
+	}	
+	return blocks * 264;
+}
+
+function calculatesFarePrice(start, end) {
+	feet = distanceTravelledInFeet(start,end);
+	charge = feet - 400;
+
+	if (feet <= 400) {
+		return 0;
+	}
+	if (feet > 400 && feet <= 2000) {
+		return charge * .02;
+	} else if (feet > 2000 && feet <= 2500) {
+		return 25;
+	} else {
+		return "cannot travel that far";
+	}
+}
