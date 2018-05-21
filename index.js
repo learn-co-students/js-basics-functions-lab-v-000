2e075de2a66dfde0ae6890b54411684c42fd1a52
nmlstyl <joey.michalina.mariano@gmail.com>
2018-05-21 19:04:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  let result = block - 42
+  if (result < 0) {
+    result = -result
+  }
+  return result
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264
+}
+
+function distanceTravelledInFeet(blockOne, blockTwo) {
+  let result
+  if (blockOne > blockTwo) {
+    result = (blockOne - blockTwo)*264
+  } else {
+    result = (blockTwo - blockOne)*264
+  }
+  return result
+}
+
+function calculatesFarePrice(start, destination) {
+  feet = distanceTravelledInFeet(start, destination
+  switch (feet) {
+    case
+  }
+}
