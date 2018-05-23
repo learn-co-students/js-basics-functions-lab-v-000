a26f3a2241e81a4b5fdbec789d7227cd75f98a27
funemployeddad <funemployeddad@gmail.com>
2018-05-22 17:00:28 -0700

    distanceFromHqInFeet complete

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (address){
-    return Math.abs(address - 42); 
+    return Math.abs(address - 42);
+}
+
+function distanceFromHqInFeet (address) {
+  return distanceFromHqInBlocks(address) * 264;
 }
