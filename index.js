696863924f983dd615aef242ce0c9116a4843495
github username <github email address>
2018-05-22 18:38:49 -0700

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ function distanceTravelledInFeet(start, end) {
   return Math.abs(start - end) * 264;
 }
 
-function calculatesFarePrice (start, end) {
-  
+function calculatesFarePrice (start, end){
+  const distance = distanceTravelledInFeet(start, end);
+
+  if (distance <= 400){
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance-400);
+  } else if (distance > 2000 && distance < 2500){
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
 }
