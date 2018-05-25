742c18fb4562f37bfc3d86562e5557e202c625f3
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-05-25 11:02:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    return Math.abs(42 - pickupLocation)
+};
+
+function distanceFromHqInFeet(pickupLocation){
+    return Math.abs(distanceFromHqInBlocks(pickupLocation)*264)
+};
+
+function distanceTravelledInFeet(start, destination){
+    const block = 264;
+    return Math.abs((start - destination)*block)
+};
+
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination);
+    let fare; 
+        if (feet <= 400){
+            fare = 0
+        }else if (feet > 400 && feet <= 2000){
+            fare = (feet - 400) * 0.02
+        }else if (feet > 2000 && feet <= 2500){
+            fare = 25;
+        }else if (feet > 2500){
+            fare = "cannot travel that far";
+    }
+    return fare;
+};
