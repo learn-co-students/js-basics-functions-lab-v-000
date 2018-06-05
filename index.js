ff1850edf42e7baed5f78a41ffc26d2a170b1a97
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-05 17:47:42 -0400

    ATP

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    return Math.abs(location - 42);
+}
+
+function distanceFromHqInFeet(location) {
+    return 264 * distanceFromHqInBlocks(location);
+}
+
+function distanceTravelledInFeet(start, dest){
+    return 264 * Math.abs(dest - start);
+}
+
+function calculatesFarePrice(start, dest){
+    distanceInFeet = distanceTravelledInFeet(start, dest);
+
+    if (distanceInFeet > 2500) {
+        return 'cannot travel that far';
+    }
+    else if (distanceInFeet > 2000) {
+        return 25;
+    }
+    else if (distanceInFeet > 400) {
+        return (distanceInFeet - 400) * 0.02;
+    }
+    else {
+        return 0;
+    }
+}
