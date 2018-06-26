ae027c977ec241dd034948588287e897856100d6
Michael Hirschberger <mlhirschberger@gmail.com>
2018-06-26 14:15:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location) {
+    return Math.abs(42 - location)
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const totalFeet = distanceTravelledInFeet(start, destination);
+    if (totalFeet <= 400) {
+        return 0;
+    } else if (totalFeet > 400 && totalFeet <= 2000) {
+        return (totalFeet - 400) * 0.02;
+    } else if (totalFeet > 2000 && totalFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
