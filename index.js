29c90fbcea67953eb29132ad79e8c2b8f05c16e3
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-07-01 05:47:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42)
+}
+
+function distanceFromHqInFeet(street) {
+    return Math.abs(distanceFromHqInBlocks(street) * 264)
+}
+
+function distanceTravelledInFeet(block_one, block_two) {
+    return Math.abs((block_one - block_two) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet;
+    feet = distanceTravelledInFeet(start, destination);
+    let price;
+    switch (true) {
+        case (feet < 400):
+            return price = 0;
+        case (feet > 400) && (feet <= 2000):
+            return price = (feet - 400) * .02;
+        case (feet > 2000) && feet <= 2500:
+            return price = 25
+        case (feet > 2500):
+            return 'cannot travel that far'
+    }
+}
