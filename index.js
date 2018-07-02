a5d9233109b81211765e24e63a59b73f5eb81d2f
AspenJames <aspenjames@tqca.org>
2018-07-02 10:55:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(bl) {
+  return Math.abs(bl-42);
+};
+
+function distanceFromHqInFeet(bl) {
+  return distanceFromHqInBlocks(bl) * 264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+};
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  switch (true) {
+    case distance > 2500:
+      return "cannot travel that far";
+      break;
+    case distance > 2000:
+      return 25;
+      break;
+    case distance > 400:
+      return (distance - 400) * .02;
+      break;
+    case distance < 400:
+      return 0;
+      break;
+  }
+}
