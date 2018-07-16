6afe995c1fe0b8d1beb34317ef870d0511380880
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-16 09:52:01 -0700

    first three functions passing

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    return Math.abs(pickupBlock - 42);
+}
+
+function distanceFromHqInFeet(pickupBlock) {
+  return distanceFromHqInBlocks(pickupBlock) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock){
+  return Math.abs(startBlock - endBlock) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+
+}
