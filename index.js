b0ceb2fa948678c90a7f1a79c05612157ee8efd7
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-16 10:16:30 -0700

    done

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ function distanceTravelledInFeet(startBlock, endBlock){
   return Math.abs(startBlock - endBlock) * 264;
 }
 
-function calculatesFarePrice(start, destination){
-
-}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) > 2500) {
+      return "cannot travel that far"
+    }
+    else if (distanceTravelledInFeet(start, destination) < 400) {
+      return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+      return (distanceTravelledInFeet(start, destination) - 400) * .02
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000) {
+      return 25
+    }
+  
+  }
