ece7300ea15cfeea43e9eadc5f7065fba3cbf82b
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-24 09:17:45 -0400

    Done.

--- index.js
+++ index.js
@@ -13,13 +13,22 @@ function distanceTravelledInFeet(start, destination) {
 
 function calculatesFarePrice(start, destination) {
   let distance = distanceTravelledInFeet(start, destination);
-  if (distance <= 400) {
-    return 0;
-  } else if (distance > 400 && distance <= 2000) {
-    return (distance - 400) / 50;
-  } else if (distance > 2000 && distance <= 2500) {
-    return 25;
-  } else {
+  // if (distance <= 400) {
+  //   return 0;
+  // } else if (distance > 400 && distance <= 2000) {
+  //   return (distance - 400) / 50;
+  // } else if (distance > 2000 && distance <= 2500) {
+  //   return 25;
+  // } else {
+  //   return "cannot travel that far";
+  // }
+  if (distance > 2500) {
     return "cannot travel that far";
+  } else if (distance > 2000) {
+    return 25;
+  } else if (distance > 400) {
+    return (distance - 400) / 50;
+  } else {
+    return 0;
   }
 }
