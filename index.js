3d3a9a3be1adc1eb15bb488ef764a08b3f672543
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-07-28 13:08:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const hq = 42 //street
+const isBelowDestination = function(pickup, destination=hq) { return pickup < destination ? true : false }
+const blocksToFeet = function(blocks) { return blocks * 264 }
+
+function diffInBlocks(pickup, destination=hq) {
+  return isBelowDestination(pickup, destination) ? destination - pickup : pickup - destination
+}
+const distanceFromHqInBlocks = diffInBlocks
+
+function distanceFromHqInFeet(pickup) {
+  return blocksToFeet(distanceFromHqInBlocks(pickup))
+}
+
+function distanceTravelledInFeet(pickup, destination) {
+  return blocksToFeet(diffInBlocks(pickup, destination))  
+}
+
+function calculatesFarePrice(pickup, destination) {
+  const distance = distanceTravelledInFeet(pickup, destination)
+  
+  switch (true) {
+    case distance <= 400:
+      return 0
+    case distance > 400 && distance <= 2000:
+      return 0.02 * (distance - 400)
+    case distance > 2000 && distance < 2500:
+      return 25
+    default: return 'cannot travel that far'
+  }
+}
