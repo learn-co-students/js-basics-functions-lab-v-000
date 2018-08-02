37d24a26e47d6e10cc554998e3bd1a384d8b4f77
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-08-02 12:59:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(blocks) {
+  if (hq > blocks) {
+    return hq - blocks;
+  } else {
+    return blocks - hq;
+  }
+}
+
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+  if (startingBlock > endingBlock) {
+    return (startingBlock - endingBlock) * 264;
+  } else {
+    return (endingBlock - startingBlock) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance > 2500) {
+    return 'cannot travel that far';
+  } else if (distance > 2000) {
+    return 25;
+  } else if (distance > 400) {
+    return (distance - 400) * 0.02;
+  } else {
+    return 0;
+  }
+}
