3ffa8adfaba773f36e70a5c25d5a36371fcbe221
GabrielValle91 <gvalle91@gmail.com>
2018-08-04 10:17:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+  const scuberHq = 42;
+  let distanceToHq = scuberHq - block;
+  if (distanceToHq < 0) {distanceToHq = -1 * distanceToHq}
+  return distanceToHq;
+}
+
+function distanceFromHqInFeet(block){
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, finish){
+  let blocks = start - finish;
+  if (blocks < 0) {blocks = -1 * blocks}
+  return blocks * 264;
+}
+
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination);
+  let cost;
+  if (distance <= 400) {
+    cost = 0;
+  } else if (distance <= 2000) {
+    cost = (distance - 400) * .02;
+  } else if (distance <= 2500) {
+    cost = 25;
+  } else {
+    cost = "cannot travel that far";
+  }
+
+  return cost;
+}
