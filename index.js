47c3dadbb4789342f6dc484f7d71388941f97cfd
roitblatari <roitblatari@gmail.com>
2018-08-16 23:50:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  const headquarters = 42
+
+  if (distance > headquarters) {
+    return distance - headquarters;
+  } else {
+    return headquarters -  distance;
+  }
+}
+
+function distanceFromHqInFeet(distance) {
+  return distanceFromHqInBlocks(distance) * 264 ;
+}
+
+function distanceTravelledInFeet(pickUp, currentBlack ) {
+  if (pickUp > currentBlack) {
+    return (pickUp - currentBlack) * 264 ;
+  } else {
+    return (currentBlack - pickUp) * 264 ;
+  }
+}
+
+function calculatesFarePrice(pickUp, destination) {
+  const theDistance = distanceTravelledInFeet(pickUp, destination );
+  let price
+  if (theDistance < 400) {
+    return price = 0;
+  } else if (theDistance  > 2500 ) {
+    return 'cannot travel that far';
+  } else if (theDistance  > 2000 ) {
+    return price = 25;
+  } else if (theDistance > 400 && theDistance  < 2000 ){
+    let deduction = theDistance - 400
+    return price = (deduction * 2 ) / 100;
+  }
+}
