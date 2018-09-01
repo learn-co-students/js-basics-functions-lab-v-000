524be1719c95795d9242869488639a5a998c9081
Jul Jen <juliajen.prog@gmail.com>
2018-09-01 01:05:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distance){
+  const hq = 42
+  if (distance >= hq) {
+    return distance - hq
+  } else {
+    return hq - distance
+  }
+}
+
+function distanceFromHqInFeet(distance){
+  const hq = 42
+  const convert = 264
+  if (distance >= hq) {
+    return (distance - hq) * convert
+  } else {
+    return (hq - distance) * convert
+  }
+}
+
+function distanceTravelledInFeet(distance){
+  
+}
