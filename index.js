93592723782a71a521addc6f8943660eaa041a5e
Kazumi Karbowski <kazkazinus@gmail.com>
2018-09-06 00:00:25 -0400

    Done.

--- index.js
+++ index.js
@@ -11,18 +11,19 @@ function distanceFromHqInFeet(distance){
 }
 
 function distanceTravelledInFeet(start, destination){
-    return Math.abs(destination - start)*264
+    return Math.abs(destination - start)*264;
 }
 
 function calculatesFarePrice(start, destination){
     let distance = distanceTravelledInFeet(start, destination);
+
     if (distance <= 400){
         return 0;
-    } else if (distance > 400 && distance < 2000){
+    } else if (distance > 400 && distance <= 2000){
        return (distance - 400) * 0.02;
     } else if (distance > 2000 && distance < 2500){
-        return 25
+        return 25;
     } else {
-        return 'cannot travel that far'
+        return 'cannot travel that far';
     }
 }
