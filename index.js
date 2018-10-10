da9a5bc343cac71be1c64ea105618b4d89bf2755
ElleeB <laura.trager@gmail.com>
2018-10-10 10:45:37 -0400

    Done.

--- index.js
+++ index.js
@@ -13,33 +13,21 @@ function distanceFromHqInFeet(location) {
 
 function distanceTravelledInFeet(start, destination) {
   if (start > destination) {
-    let distance = (start - destination) * 264
-    return distance
+    return (start - destination) * 264
   }
-  let distance = (destination - start) * 264
-  return distance
+  return (destination - start) * 264
 }
 
 function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination)
 
   if (distance <= 400) {
-    let fare = 0
-    return fare
-  }
-
-  if (distance > 400 && distance <= 2000 ) {
-    let fare = (distance - 400) * .02
-    return fare
-  }
-
-  if (distance > 2000 && distance < 2500) {
-    let fare = 25
-    return fare
-  }
-
-  if (distance > 2500) {
-    let fare = 'cannot travel that far'
-    return fare
+    return 0
+  } else if (distance > 400 && distance <= 2000 ) {
+    return (distance - 400) * .02
+  } else if (distance > 2000 && distance < 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
   }
 }
