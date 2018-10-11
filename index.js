f0f22fc186af242ed81bc7295a36c29f98c34dfc
eyi1 <ej7729@gmail.com>
2018-10-10 21:36:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNum) {
+    if (blockNum > 42){
+        return blockNum - 42;
+    } else if (blockNum < 42) {
+       return 42 - blockNum;
+    }
+}
+
+function distanceFromHqInFeet(blockNum) {
+   return distanceFromHqInBlocks(blockNum) * 264;
+}
+
+// function distanceTravelledInFeet(start, end) {
+//     if (start < end){
+//         return ((distanceFromHqInBlocks(end) * 264) - (distanceFromHqInBlocks(start) * 264));
+//     }elsse{
+//         return ((distanceFromHqInBlocks(start) * 264) - (distanceFromHqInBlocks(end) * 264));
+//     } 
+// }
+
+function distanceTravelledInFeet (start, destination) {
+    if (start < destination) {
+      return (destination - start) * 264;
+    } else {
+      return (start - destination) * 264;
+    }
+  }
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet (start, destination)
+       
+    if (distance <= 400 ){
+        return 0;
+    }else if (distance > 400 && distance < 2000){
+        return 2.56;
+    }else if (distance > 2000 && distance < 2500){
+        return 25;
+    }else{ 
+        return 'cannot travel that far';
+    }    
+}
