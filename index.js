300ed5689ed4c66b356f144df11748926608aac7
Jonathan Chan <johnkchan252@gmail.com>
2018-10-30 03:02:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+  if (distance >= 42){
+    return distance - 42;
+  }
+  return 42 - distance;
+}
+
+function distanceFromHqInFeet(distance){
+  return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(start, end){
+  return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end){
+  if (distanceTravelledInFeet(start, end) < 400){
+    return 0;
+  } else if (distanceTravelledInFeet(start, end) >= 400 && distanceTravelledInFeet(start, end) <= 2000){
+    return (distanceTravelledInFeet(start, end) - 400) * .02;
+  } else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end) < 2500){
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
