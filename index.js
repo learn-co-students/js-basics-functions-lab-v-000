266e52f706fef997067310505c6382d6fe6a0e17
BrentBauman86 <brentbauman86@gmail.com>
2018-11-15 13:12:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceTravelledInFeet(){
+
+}
+
+function distanceFromHqInBlocks(){
+
+}
+
+function distanceFromHqInFeet(){
+
+}
+
+function calculatesFarePrice(start, destination){
+
+}
