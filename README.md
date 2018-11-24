3b2c728ae76eaffe7f81c913dc0875f11f5b8c2d
Ayana Bando <ayanaflax@yahoo.com>
2018-11-24 16:17:29 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Functions Lab
+ # Functions Lab
 
 ## Problem Statement
 
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+let dist;
+ 
+if (block > 42) {
+  dist =  (block - 42);
+} else if (block < 42) {
+  dist = (42 - block);
+}
+return dist;
+}
+
+function distanceFromHqInFeet(feet) {
+  let block = 264
+  dist = distanceFromHqInBlocks(feet) * 264;
+  return dist;
+}
+
+function distanceTravelledInFeet(hq, block) {
+  let dist; 
+  if (block > hq) {
+  dist = ((block-hq) *264);
+  } else if (hq > block){
+  dist = ((hq-block) *264);
+  }
+  return dist;
+
+}
+
+function calculatesFarePrice(start, destination){
+  let total = distanceTravelledInFeet(start, destination)-400;
+  let cost = 0.02;
+
+  if (distanceTravelledInFeet(start, destination) <= 400) {
+    return 0;
+  } else if (total <= 1600) {
+    return cost * total;
+  } else if (total <= 2000) {
+    return 25;
+  } else if (total <= 2500 ) {
+    return 'cannot travel that far';
+  }
+  
+}
