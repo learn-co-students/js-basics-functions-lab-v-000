06c4f4dcdd85b99c6e891f71cde9b22a96722e20
github username <github email address>
2018-12-05 15:42:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(distance) {
-  var blocks = distance - 42;
-  if (blocks > 0) {
-    return blocks;
-} else {
-    return -(blocks);
-}
-
-
+  let blocks = distance - 42;
+    if (blocks > 0) {
+      return blocks;
+    }
+    else {
+      return -(blocks);
+    }
 }
 
 function distanceFromHqInFeet(distance) {
-  return distanceFromHqInBlocks(distance)* 264;
+  return distanceFromHqInBlocks(distance) * 264
 }
 
-function distanceTravelledInFeet(distanceOne, distanceTwo) {
-  return -(distanceFromHqInFeet(distanceOne) - distanceFromHqInFeet(distanceTwo));
-
-}
-function calculatesFarePrice(distanceOne, distanceTwo) {
-  var totalDistance = distanceTravelledInFeet(distanceOne, distanceTwo);
-  if (totalDistance < 400) {
-    return 0;
-  } else if ((totalDistance > 400) && (totalDistance < 2000)) {
-    return (totalDistance - 400) * 0.02;
+function distanceTravelledInFeet(distance1, distance2) {
+  let blocks = distance2 - distance1
+  if (blocks > 0) {
+    return blocks * 264
   }
-    else if ((totalDistance > 2000) && (totalDistance < 2500)) {
+  else {
+    return -(blocks) *264
+  }
+}
+
+function calculatesFarePrice(start, end) {
+  distanceTravelled = distanceTravelledInFeet(start, end)
+  if (distanceTravelled <= 400) {
+    return 0;
+  }
+  else if (distanceTravelled > 400 && distanceTravelled < 2000) {
+    return (distanceTravelled- 400) * 0.02
+  }
+  else if (distanceTravelled > 2000 && distanceTravelled < 2500) {
     return 25;
   }
-  else  {
-    return 'cannot travel that far';
+  else {
+      return 'cannot travel that far'
   }
 }
