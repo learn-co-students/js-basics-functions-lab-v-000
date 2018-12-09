da87116bba849dad80fae55b94d2ec30160c605b
Christopher P. Dundon <chris.dundon@gmail.com>
2018-12-08 23:21:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(loc) {
+	let dist;
+
+	dist = Math.abs(loc - 42);
+
+	return dist;
+}
+
+function distanceFromHqInFeet(loc) {
+	return distanceFromHqInBlocks(loc) * 264;
+}
+
+function distanceTravelledInFeet(locA, locB) {
+	const dist = Math.abs(locB - locA) * 264;
+
+	return dist;
+}
+
+function calculatesFarePrice(start, destination) {
+	const billableDist = (distanceTravelledInFeet(start, destination) - 400); //the first 400 feet are free.
+	let px;
+
+	if (billableDist <= 0) {
+		px = 0;
+	} else if (billableDist <= 1600) {
+		px = billableDist * 0.02;
+	} else if (billableDist <= 2100) {
+		px = 25;
+	}	else {
+		px = 'cannot travel that far';
+	}
+
+	return px;
+}
