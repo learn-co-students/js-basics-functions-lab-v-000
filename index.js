3ca8e8f9e26e986cdb97bc7880afd254b307e2df
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2018-12-16 03:10:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+  distance = (street - 42) > 0 ? street - 42 : 42 - street
+  return distance
+}
+
+function distanceFromHqInFeet(street){
+  return distanceFromHqInBlocks(street) * 264
+}
+
+function distanceTravelledInFeet(s1, s2){
+  ft = (s2 - s1) * 264 > 0 ? (s2 - s1) * 264 : (s1 - s2) * 264
+  return ft
+}
+
+function calculatesFarePrice(start, destination){
+  d = distanceTravelledInFeet(start, destination)
+  if (d <= 400){
+    return 0
+  } else if (400 < d && d < 2000){
+    return .02 * (d - 400)
+  } else if (2000 <= d && d < 2500){
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
