154ab6172a4c44bb553518aca956ac47ef59ee90
Keith <fkmccallion@gmail.com>
2019-01-08 06:21:45 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  let result;
+
+  if (someValue > 42) {
+    result = someValue - 42;
+  }
+  else {
+    result = 42 - someValue;
+  }
+
+  return result;
+}
+
+function distanceFromHqInFeet(somevalue) {
+  let result = distanceFromHqInBlocks(somevalue) * 264;
+
+  return result;
+}
+
+function distanceTravelledInFeet(someValue) {
+  let result = distanceFromHqInFeet(someValue);
+
+  return someValue;
+}
