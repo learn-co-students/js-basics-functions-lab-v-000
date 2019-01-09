e87ce430b1594a5423725cb56b9c20fec9ea2551
chrissygonzalez <chrissygonzalez@gmail.com>
2019-01-09 09:50:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance <= 42){
+        return 42 - distance;
+    } else {
+        return distance - 42;
+    }
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInBlocks(start, end){
+    if (start <= end){
+        return end - start;
+    } else {
+        return start - end;
+    }
+}
+
+function distanceTravelledInFeet(start, end){
+    return distanceTravelledInBlocks(start, end) * 264;
+}
+
+function calculatesFarePrice(start, end){
+    let trip = distanceTravelledInFeet(start, end);
+    if (trip > 2500) {
+        return 'cannot travel that far'
+    } else if (trip < 400) {
+        return 0;
+    } else if (trip > 2000) {
+        return 25;
+    } else {
+        return (trip - 400) * 0.02;
+    }
+}
