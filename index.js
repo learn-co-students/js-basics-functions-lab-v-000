50e60c85475f7c3be6b6bd61e9c89c8921680c9b
lauradarlak <lauradarlak@gmail.com>
2019-01-16 15:12:30 -0500

    Done.

--- index.js
+++ index.js
@@ -16,15 +16,24 @@ function distanceTravelledInFeet(start, destination) {
 
 function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination)
-  let farePrice
 
   if (distance < 400) {
-    return farePrice = 0;
+    return 0;
   } else if (distance <= 2000 && distance >= 400) {
-    return farePrice = (distance - 400) * .02;
+    return (distance - 400) * .02;
   } else if (distance <= 2500 && distance > 2000) {
-    return farePrice = 25;
-  } else if (distance > 2500) {
+    return 25;
+  } else {
     return 'cannot travel that far';
   }
 }
+
+// Learn.co solution below. Is Math.abs() inappropriate? Passes.
+
+// function distanceFromHqInBlocks (blockNumber) {
+//   if (blockNumber > 42) {
+//     return blockNumber - 42;
+//   } else {
+//     return 42 - blockNumber;
+//   }
+// }
