c385c7b754c3ac1373c715aed96568597bb8249c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-27 22:20:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+  if (someValue >= 42) {
+    return someValue - 42;}
+    else 
+      return 42 - someValue;
+}
+
+function distanceFromHqInFeet (someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  if (startBlock > endBlock) {
+    return (startBlock - endBlock) * 264;}
+      else {
+      return (endBlock - startBlock) * 264;}
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+  distance = distanceTravelledInFeet(startBlock, endBlock);
+  
+  if (distance <= 400) {
+    return 0;}
+    else if (400 < distance && distance <= 2000) {
+    return (distance - 400) * 0.02;}
+      else if (2000 < distance && distance < 2500) {
+      return 25;}
+        else {
+          return 'cannot travel that far';}
+}
+
+
