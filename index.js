55e7b0ca9a0726c53055f5c1e393eb9dc9af9bc6
Pamela Frieze <pfrieze@gmail.com>
2019-01-29 20:07:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks (BlockDistance) {
-  if (BlockDistance > 42) {
-    return BlockDistance - 42;
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
   } else {
-  return 42 - BlockDistance;
+    return 42 - blockNumber;
   }
 }
 
-function distanceFromHqInFeet (distanceFromHqInBlocks) {
-  (distanceFromHqInBlocks)* 264
-  console.log(result);
+function distanceFromHqInFeet (blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
-function calculatesFarePrice(distanceFromHqInFeet) {
-  if (distanceFromHqInFeet < 400) {
-    console.log(0);
-  } else if (distanceFromHqInFeet > 400 && < 2000) {
-    console.log(.02 * distanceFromHqInFeet);
+
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
   }
-   else if (distanceFromHqInFeet > 2000 && < 2500) {
-    console.log(25);
-   }
-   else {
-     console.log ("cannot travel that far")
-   }
 }
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
+
