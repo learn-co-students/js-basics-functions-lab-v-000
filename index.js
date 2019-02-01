e19029a87a3826fe588ba4baedf388839a09ab02
amandena <manda.panda91@yahoo.com>
2019-01-31 23:03:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (street) {
+  let hq = 42;
+  if (street > hq) {
+    return (street - hq);
+  } else {
+    return (hq - street);
+  }
+}
+
+function distanceFromHqInFeet (street) {
+  return (distanceFromHqInBlocks(street) * 264);
+}
+
+function distanceTravelledInFeet (start, end) {
+  if (start > end) {
+    return (start - end) * 264;
+  } else {
+    return (end - start) * 264;
+  }
+}
+
+function calculatesFarePrice (start, destination) {
+  const feet = distanceTravelledInFeet(start, destination);
+  if (feet <= 400) {
+    return 0;
+  }
+  else if (feet > 400 && feet <= 2000) {
+    return (feet - 400) * .02;
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    return 25;
+  }
+  else if (feet > 2500) {
+    return "cannot travel that far";
+  }
+}
