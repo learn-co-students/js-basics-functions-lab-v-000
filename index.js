e9d92cb5443bccb664fad0bf3ebd9aacf51fd91d
Rebecca Strauss <rstrauss127@gmail.com>
2019-02-10 23:29:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(x) {
-  distance = Math.abs(42 - x);
+  let distance = Math.abs(42 - x);
 }
 
 function distanceTravelledInFeet(distance) {
-  feet = distance * 264;
+  let feet = distance * 264;
 }
