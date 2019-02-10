3e8ef209dc204de919b429d6e238356ad483b6b7
Rebecca Strauss <rstrauss127@gmail.com>
2019-02-10 23:47:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function distanceTravelledInFeet(x, y) {
   let distance = Math.abs(x-y)*264;
   return distance;
 }
+
+function calculatesFarePrice(x, y) {
+  let price = 0;
+  let distance = distanceTravelledInFeet(x, y);
+  
+    if (distance > 2500) {
+      price = "cannot travel that far";
+    } else if (distance > 2000) {
+      price = 25;
+    } else if (distance > 401) {
+      price = (distance - 401) * 0.02;
+    }
+  return price;  
+  }
+  
