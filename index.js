bc1ca5ca5cb722aca6c622a6240fa7ecd17bde03
Christopher Beltran <christod.beltran@gmail.com>
2019-03-17 20:45:29 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  const hq = 42;
+  if (location > hq) {
+    return location - hq;
+  }
+  else if (location < hq) {
+    return hq - location;
+  }
+}
+
+function distanceFromHqInFeet(location) {
+  let blocks = distanceFromHqInBlocks(location);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (end > start) {
+    return (end - start) * 264;
+  } else if (start > end) {
+    return (start - end) * 264;
+  }
+}
+
+function calculatesFarePrice(start, end) {
+  let feet = distanceTravelledInFeet(start, end);
+  if (feet < 400) {
+    return 0;
+  }
+  else if (feet > 400 && feet <= 2000) {
+    return (feet - 400) * 0.02
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    return 25;
+  }
+  else if (feet > 2500) {
+    return 'cannot travel that far';
+  }
+}
