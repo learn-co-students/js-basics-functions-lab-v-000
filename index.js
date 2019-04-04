97cb43743e4ece4f3532c0fcc85f0d1456f8b59e
Rachel Wyatt <rw126@evansville.edu>
2019-04-04 18:21:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+  if (someValue >= 42) {
+    let result = someValue - 42;
+    return result;
+  }
+  else if (someValue < 42) {
+    let result = 42 - someValue;
+    return result;
+  }
+}
+ 
+function distanceFromHqInFeet (someValue) {
+  result = distanceFromHqInBlocks(someValue) * 264;
+  return result;
+}
+
+function distanceTravelledInFeet (streetOne, streetTwo) {
+  if (streetOne < streetTwo) {
+    let result = (streetTwo - streetOne) * 264;
+    return result;
+  }
+  else if (streetOne > streetTwo) {
+    let result = (streetOne - streetTwo) * 264;
+    return result;
+  }
+}
+
+function calculatesFarePrice (streetOne, streetTwo) {
+  distance = distanceTravelledInFeet (streetOne, streetTwo)
+  if (distance < 400) {
+    let result = 0;
+    return result;
+  }
+  else if (distance >= 400 && distance <= 2000) {
+    let result = .02 * (distance - 400);
+    return result;
+  }
+    else if (distance > 2000 && distance < 2500) {
+    let result = 25;
+    return result;
+  }
+      else if (distance > 2500) {
+    let result = 'cannot travel that far';
+    return result;
+  }
+}
