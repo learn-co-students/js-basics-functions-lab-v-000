228aef85ead77cc38daf932dbe8cd906ca448b9a
sjwakean <swakeman@ca.rr.com>
2019-04-15 15:56:31 -0700

    Done.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ function distanceFromHqInBlocks(someValue) {
     
     streetHQ = 42;
     calculation = (someValue - streetHQ);
-        if (calculation < 0)
-            return (Math.abs(calculation));
-        else
+        if (calculation < 0) { 
+            return (Math.abs(calculation)); 
+        }
+        else {
             return calculation;
+        }
 }    
      
 function distanceFromHqInFeet(someValue) {
@@ -14,30 +16,34 @@ function distanceFromHqInFeet(someValue) {
 }
 
 function distanceTravelledInFeet(start, end) {
-    calculation = (start - end) * 264;
-        if (calculation < 0)
+    calculation = (start - end) * 264; 
+        if (calculation < 0) {
             return (Math.abs(calculation));
-        else 
+        }
+        else {
             return calculation;
-}
+        }
+}       
 
 function calculatesFarePrice(start, destination) {
     distanceTravelledInFeet(start, destination)
 //(remember the first 400 feet are free!)
-        if (distanceTravelledInFeet(start, destination) <= 400) 
+        if (distanceTravelledInFeet(start, destination) <= 400) {
             return 0;
-           
+        }
 //charges 2 cents per foot when total feet travelled is between 400 and 2000 
-        else if (distanceTravelledInFeet(start, destination) <= 2000)
-            amount = (distanceTravelledInFeet(start, destination) - 400) * .02;
-                return amount;
-
+        else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        amount = (distanceTravelledInFeet(start, destination) - 400) * .02;
+            return amount;
+        }
 //charges 25 dollars for a distance over 2000 feet
-        else if (distanceTravelledInFeet(start, destination) < 2500)
+        else if (distanceTravelledInFeet(start, destination) < 2500) {
             return 25;
-
+        }
 //does not allow rides over 2500 feet
-        else    
+        else {   
             return "cannot travel that far";
+        }
 }
+
 
