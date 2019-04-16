d1d1af60d46c69d9d93a0ada2de1f6d932ddedf8
mattetress <mattetress@me.com>
2019-04-16 15:30:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+  if (destination > 42) {
+    return (destination - 42);
+  } else {
+    return (42 - destination);
+  }
+}
+
+function distanceFromHqInFeet(destination) {
+  return (distanceFromHqInBlocks(destination) * 264);
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  if (startBlock < endBlock) {
+    return ((endBlock - startBlock) * 264);
+  } else {
+    return ((startBlock - endBlock) * 264);
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return ((distance - 400) * 0.02);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
