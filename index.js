e7ab756d6e23bf6c6f934e14d6fff1cdcd68b9a9
github username <github email address>
2019-04-18 22:26:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+ s// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber){
+  let distance = blockNumber - 42;
+  return Math.abs(distance);
+}
+
+function distanceFromHqInFeet(blockNumber){
+  let feet = distanceFromHqInBlocks(blockNumber)*264;
+  return feet;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock){
+  let blocksTravelled = Math.abs(startBlock - endBlock);
+  let distanceInFeet = blocksTravelled * 264;
+  return distanceInFeet;
+}
+
+function calculatesFarePrice(start, destination){
+  let distance = distanceFromHqInFeet(start, destination);
+  let price;
+  switch (distance){
+    case(distance < 400){
+      price = 0;
+    }
+  }
+  return price;
+}
