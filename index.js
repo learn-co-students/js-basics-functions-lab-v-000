1eeb3db051b5f129d2048d09b10d33e8859f1009
Taiwei Ko <mike25ko@gmail.com>
2019-05-10 20:52:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  const hq = 42
+  let result
+  if (street < hq) {
+    result = hq - street;
+  } else {
+    result = street - hq;
+  }
+    return result
+}
+
+ function distanceFromHqInFeet(street) {
+  let distance
+  distance = distanceFromHqInBlocks(street) * 264;
+  return distance
+}
+
+ function distanceTravelledInFeet(start, destination) {
+  let distance
+  if (start > destination) {
+    distance = (start - destination) * 264;
+  } else {
+    distance = (destination - start) * 264;
+  }
+  return distance
+}
+
+ function calculatesFarePrice(start, destination){
+  let fare
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    fare = 0;
+  } else if (distance > 400 && distance <= 2000 ){
+    fare = (distance - 400) * 0.02;
+  } else  if (distance > 2000 && distance < 2500){
+    fare = 25;
+  } else {
+    fare = 'cannot travel that far';
+  }
+  return fare
+}
