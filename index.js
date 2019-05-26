99a22e9f853f0aeec035476a9f2b039e000956b4
brennanmrose <brennan.m.rose@gmail.com>
2019-05-26 12:41:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,41 +1,33 @@
-function distanceFromHqInBlocks(blockNumber) {
-	let result
-	if (blockNumber > 42) {
-		result = blockNumber - 42
-	} else if (blockNumber < 42) {
-		result = 42 - blockNumber
-	}
-	return result
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
 }
 
 function distanceFromHqInFeet(blockNumber) {
-	distanceFromHqInBlocks(blockNumber);
-	return distanceFromHqInBlocks(blockNumber) * 264
+	return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
 function distanceTravelledInFeet(endingBlock, startingBlock) {
-	let result
 	if (startingBlock > endingBlock) {
-		result = (startingBlock - endingBlock) * 264
-	} else if (startingBlock < endingBlock) {
-		result = (endingBlock - startingBlock) * 264
+		return (startingBlock - endingBlock) * 264;
+	} else {
+		return (endingBlock - startingBlock) * 264;
 	}
-	return result
 }
 
 function calculatesFarePrice(endingBlock, startingBlock) {
-	distanceTravelledInFeet(endingBlock, startingBlock);
-	let feet
-	let result
-	feet = distanceTravelledInFeet(endingBlock, startingBlock)
+	const feet = distanceTravelledInFeet(endingBlock, startingBlock);
+
 	if (feet < 400) {
-		result = 0
+		return 0;
 	} else if (feet > 400 && feet < 2000) {
-		result = (feet - 400) * 0.02
+		return (feet - 400) * 0.02;
 	} else if (feet > 2500) {
-		result = 'cannot travel that far'
+		return 'cannot travel that far';
 	} else if (feet > 2000) {
-		result = 25
+		return 25
 	}
-	return result
 }
