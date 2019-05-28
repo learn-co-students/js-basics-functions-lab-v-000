ce11b6d2fcb769909d9f977e54b9b98dc62c1cd3
MargaritaKhodzka <mkhodzka@gmail.com>
2019-05-28 13:04:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+  if (streetNumber > 42) {
+    return streetNumber - 42;
+  } else {
+    return 42 - streetNumber;
+  };
+};
+
+function distanceFromHqInFeet(streetNumber) {
+  let blocks = distanceFromHqInBlocks(streetNumber);
+  return blocks * 264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+  if (start > destination) {
+    return (start - destination) * 264;
+  } else {
+    return (destination - start) * 264;
+  };
+};
+
+function calculatesFarePrice(start, destination) {
+  let distance  = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return 0.02 * (distance - 400);
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  };
+};
