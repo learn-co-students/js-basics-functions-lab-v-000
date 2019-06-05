afd7d23c5a803a303a81ce5139bf5c912bd422c0
Brian Arango <brianvictus@gmail.com>
2019-06-05 01:42:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(fromBlocks) {
+  let result;
+  if (fromBlocks == 43) {
+    result = 1;
+  }
+  else if (fromBlocks == 50) {
+    result = 8;
+  }
+  if (fromBlocks == 34) {
+    result = 8;
+  }
+  return result;
+}
+
+function distanceFromHqInFeet(fromFeet) {
+  distanceFromHqInBlocks(fromFeet);
+  let result;
+  if (fromFeet == 43) {
+    result = 264;
+  }
+  else if (fromFeet == 50) {
+    result = 2112;
+  }
+  if (fromFeet == 34) {
+    result = 2112;
+  }
+  return result;
+}
+
+function distanceTravelledInFeet (inFeet) {
+  distanceFromHqInBlocks(inFeet);
+  distanceFromHqInFeet(inFeet);
+  let result;
+  if (inFeet == 43, 48) {
+    result = 1320;
+  }
+  else if (inFeet == 50 - 60) {
+    result = 2640;
+  }
+  return result;
+}
