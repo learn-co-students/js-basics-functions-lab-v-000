9d00723f73443211b57496868a74117124499b23
corytveten <corytveten@gmail.com>
2019-07-06 10:17:04 -0700

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ function distanceFromHqInFeet(someValue) {
 }
 
 function distanceTravelledInFeet(start, end) {
-    blocks = Math.abs(start - end);
+    let blocks = Math.abs(start - end);
     return blocks * 264;
+}
+
+function calculatesFarePrice(start, end) {
+    let dist = distanceTravelledInFeet(start, end);
+    
+    if (dist <= 400) {
+        return 0;
+    } else if (dist > 400 && dist < 2000) {
+        return (dist - 400) * .02;
+    } else if (dist >= 2000 && dist < 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+    
 }
