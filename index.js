759c724f04fa197c5f65e8f2a87397813720fb87
Heshie Brody <4hbrody@gmail.com>
2019-07-09 19:44:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks (street) {
+  return street < hq ? hq - street : street - hq;
+}
+
+function distanceFromHqInFeet (street) {
+  const blocks = distanceFromHqInBlocks(street);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet (startB, endB) {
+  const distance = startB > endB ? startB - endB : endB - startB;
+  return distance * 264;
+}
+
+function calculatesFarePrice (startB, endB) {
+  const feet = distanceTravelledInFeet(startB, endB);
+
+  if (feet > 2500) {
+    return "cannot travel that far";
+  } else if (feet > 2000) {
+    return 25;
+  } else if (feet > 400) {
+    return (feet - 400) * 0.02;
+  } else {
+    return 0;
+  }
+}
