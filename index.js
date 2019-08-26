bbbcd215c533b62e2db6046f1826156fdebdbbbe
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-08-26 17:44:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  const hq = 42
+  const locationBeforeOrAfter42 = [location, hq].sort()
+  return locationBeforeOrAfter42[1] - locationBeforeOrAfter42[0]
+}
+
+function distanceFromHqInFeet(location) {
+  const distancePerBlock = 264;
+  return distanceFromHqInBlocks(location) * distancePerBlock
+}
+
+function distanceTravelledInFeet(start, end) {
+  const numOfBlocksTravelled = [start, end].sort()[1] - [start, end].sort()[0]
+  return numOfBlocksTravelled * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceTravelled = distanceTravelledInFeet(start, destination)
+  if (distanceTravelled <= 400) {
+    return 0;
+  } else if (distanceTravelled > 400 && distanceTravelled < 2000) {
+    return (distanceTravelled - 400) * .02
+  } else if (distanceTravelled > 2000 && distanceTravelled < 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
