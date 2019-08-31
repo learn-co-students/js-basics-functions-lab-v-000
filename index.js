50ab4b6b75abf033aa7e04aa1af6ab7ca0d9bf66
Rebecca Tong <rtong@buffalo.edu>
2019-08-31 16:26:07 -0400

    did two functions

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+let distance;
+if (someValue >= 42){
+distance = someValue - 42}
+else{
+distance = someValue - 26
+}
+return distance
+}
+
+function distanceFromHqInFeet (someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+  // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  //the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+
+function distanceTravelledInFeet(blockEnded,blockStarted){
+  let blocks;
+  let distance;
+  if blockStarted < blockEnded {
+    blocks = blockStarted - blockEnded
+    distance = blocks * 264;
+  }
+    else {
+      blocks = blockStarted - blockEnded
+      distance = blocks * 264;
+    }
+return distance
+}
+
+function calculatesFarePrice(someValue){
+
+}
