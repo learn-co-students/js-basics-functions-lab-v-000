65aaf239ab39f27be0c51db8dc218ae1f4c8a2fa
Kat Tochowicz <ktochow1@binghamton.edu>
2019-09-09 12:23:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+  if (streetNumber < 42) {
+    return 42 - streetNumber
+  } else if (streetNumber > 42) {
+    return streetNumber - 42
+  } else {
+    return streetNumber
+  }
+
+}
